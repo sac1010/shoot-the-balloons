bed321341f2a7e27d7417d066d5111a5a372310c
Sachin Girish <83954950+sac1010@users.noreply.github.com>
2022-05-28 15:17:07 +0530

    added logic to move ballons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Balloons from './components/Balloons';
 import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   
@@ -9,6 +13,7 @@ function App() {
   const [added, setAdded] = useState([])
   const [shootNum, setShootNum] = useState("")
 
+  //generates an array of random colors
   const colorGen = ()=>{
     let arr = []
     for(let i=1; i<=5; i++){
@@ -21,10 +26,11 @@ function App() {
     colorGen()
   }, [])
 
+  //to shoot the balloons to empty div
   const handleClick = ()=>{
    let balloon = balloons.find((el)=>el[1]==shootNum)
    if(!balloon){
-     alert("please enter a valid number")
+     toast.error("enter a valid number")
      return
    }
    const newArr = balloons.filter((el)=>el[1]!=shootNum)
@@ -32,19 +38,35 @@ function App() {
    setAdded([...added, balloon])
   }
 
+  //to add the balloon back to original container
+  const handleAdded = (el)=>{
+    let newArr = added.filter((elem)=>el[1]!==elem[1])
+    setAdded([...newArr])
+    setBalloons([...balloons, el].sort((a,b)=>a[1]-b[1]))
+  }
+
   return (
     <div className="app">
       <div className="main">
-        {balloons.map((el)=><Balloons color={el[0]} num={el[1]}></Balloons>)}
+        {balloons.map((el)=><Balloons key={el[1]} color={el[0]} num={el[1]}></Balloons>)}
       </div>
-      <div className="empty" style={{display:"flex", gap:"10px", flexWrap:"wrap"}}>
-      {added.map((el)=><Balloons color={el[0]} num={el[1]}></Balloons>)}
+
+      <div className="empty" style={{display:"flex", gap:"10px", flexWrap:"wrap", padding:"10px"}}>
+      {added.map((el)=>{
+        return(
+          <div  onClick={()=>handleAdded(el)}>
+            <Balloons key={el[1]} color={el[0]} num={el[1]}></Balloons>
+          </div>
+        )
+      })}
       </div>
+
       <div className="shoot">
-          <input onChange={(e)=>setShootNum(e.target.value)} type="number" />
-          <button onClick={handleClick}>shoot</button>
+      <TextField  onChange={(e)=>setShootNum(e.target.value)} id="outlined-basic" label="enter a number" variant="outlined" type="number" />
+          <Button onClick={handleClick} variant='contained'>shoot</Button>
       </div>
     </div>
+    
   );
 }
 
--- src/components/Balloons.jsx
+++ src/components/Balloons.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
-const Balloons = ({color, num}) => {
-    
+import "./balloons.css"
+const Balloons = ({color, num}) => {    
   return (
-    <div style={{width:"100px", height:"100px", borderRadius:'50%', background:color, display:"flex", justifyContent:"center", alignItems:"center"}}>
+    <div className='balloons' style={{width:"100px", height:"100px", borderRadius:'50%', background:color, display:"flex", justifyContent:"center", alignItems:"center"}}>
         <div>{num}</div>
     </div>
   )
